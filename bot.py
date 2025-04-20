606a57a4eacaab3b043eacfe81ef1b86f2597c01
sell <sell@MacBook-Pro-sell.local>
2025-04-20 21:26:46 +0300

    Обновление

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
-WEBAPP_URL = "https://gulyai-webapp.vercel.app/"
+WEBAPP_URL = "https://gulyai-webapp.vercel.app"
 ADMIN_ID = 987664835
 
 db = PostgrestClient(f"{SUPABASE_URL}/rest/v1")
@@ -194,13 +194,10 @@ async def handle_meet_response(update: Update, context: ContextTypes.DEFAULT_TYP
     from_id = data.split("_")[1]
 
     if data.startswith("agree_"):
-        initiator = await context.bot.get_chat(from_id)
-        initiator_username = initiator.username
-        initiator_id = initiator.id 
-        if initiator.username:
-            text = f"✅ Вы согласились! Вот ссылка: [@{initiator.username}](https://t.me/{initiator.username})",
+        await query.message.reply_text(
+            f"✅ Вы согласились! Вот ссылка: [t.me/{from_id}](https://t.me/{from_id})",
             parse_mode="Markdown"
-        
+        )
         try:
             await context.bot.send_message(
                 chat_id=from_id,
@@ -212,7 +209,7 @@ async def handle_meet_response(update: Update, context: ContextTypes.DEFAULT_TYP
 
     elif data.startswith("decline_"):
         await query.message.reply_text("❌ Вы отклонили предложение.")
-
+        
 # Инициализация
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
